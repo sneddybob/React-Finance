b545655ea9f85a5cd96a2d53bf545adbd5fc614a
sneddybob <bobsned12@gmail.com>
2019-03-04 16:35:33 -0600

    added navbar component functionality with ipex json

--- src/navbar.js
+++ src/navbar.js
@@ -1,31 +1,68 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
-  render() {
+    constructor(){
+        super();
+        this.state = {
+            allSymbols : [],
+            searchValue : "",
+            matchedSymbols : []
+        }
+    }
+    componentDidMount(){
+        fetch('https://api.iextrading.com/1.0/ref-data/symbols').then((response) => {
+            response.json().then((data) =>{
+                //Setting state on a component should kick-off the update lifecycle
+                this.setState(state => {return {allSymbols: data }});
+            })
+        })
+    }
+
+    searchOnSubmit = function (e) {
+        e.preventDefault();
+        var searchValue = document.getElementsByName('search')[0].value.toLowerCase();
+      
+        this.setState(state => {return {searchValue: searchValue }});        
+        var matchedSymbols = this.state.allSymbols.filter(function(e) { return e.symbol.toLowerCase() === searchValue || e.name.toLowerCase().indexOf(searchValue) >= 0 });
+
+        this.setState(state => {return {matchedSymbols: matchedSymbols }});
+    }
+    
+  
+    render() {
+        var counter = this.state.matchedSymbols.length; 
     return (
-      <div><nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">React-Finance</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">React-Finance</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Stock Symbols</a>
+          <li className="nav-item">
+            <a className="nav-link" href="/">Stock Symbols</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Compare Symbols</a>
+          <li className="nav-item">
+            <a className="nav-link" href="/">Compare Symbols</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
-          </li>
+          
         </ul>
+        <form className="form-inline" onSubmit={e => this.searchOnSubmit(e)}>
+    <input name ="search" list = "symbols" className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+     <span className="input-group-text" id="basic-addon1">Number of results {counter}</span>
+    <datalist id="symbols">
+    {this.state.matchedSymbols.map(function(e){return <option key={e.symbol} value={e.symbol}>{e.name}</option> })}
+    </datalist>
+    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
       </div>
-    </nav></div>
+    </nav>
+    
     );
+    
   }
 }
 
